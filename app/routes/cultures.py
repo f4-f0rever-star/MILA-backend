6cb55daba86be1b4e2c9cde8b9f9e4907e4548a8
Faizah <lolcanyouseeme@gmail.com>
2026-05-18 21:11:38 +0300

    Updated culture.py, history.py and traditions.py

--- app/routes/cultures.py
+++ app/routes/cultures.py
@@ -223,7 +223,7 @@ def get_culture(culture_id):
 
     return jsonify({
         "culture_id": culture_id,
-        "name": "Culture",
+        "name": "Kenya",
         "food": food_data,
         "country": country_data,
         "art": art_data,
